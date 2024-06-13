99fba518f129bc567724f037e07367f35cae5b3e
FourFlames <74077488+FourFlames@users.noreply.github.com>
2024-06-13 13:22:46 -0700

    Can read lines of config file now

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,4 +1,5 @@
 use std::fs::{self, create_dir};
+use std::io::{self};
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
@@ -6,15 +7,37 @@ pub struct Files {
     config_root: PathBuf,
     config_files: Vec<PathBuf>,
     data_root: PathBuf,
-    data_files: Vec<PathBuf>,
+    _data_files: Vec<PathBuf>,
 }
 
-pub fn initialize() -> std::io::Result<Files> {
+impl Files {
+    pub fn read_lines_of(&self, name: &str) -> io::Result<Vec<String>> {
+        let config_file = self.config_files.iter().find_map( |p| {
+            let mut pb = PathBuf::from(&self.config_root);
+            pb.push(name);
+            pb.set_extension("cfg");
+            if p == &pb {
+                Some(p)
+            } else {
+                None
+            }
+        });
+
+        if let Some(path) = config_file {
+            let content = fs::read_to_string(path)?;
+            Ok(content.lines().map(String::from).collect())
+        } else {
+            Err(io::Error::new(io::ErrorKind::NotFound, "Config file not found"))
+        }
+    }
+}
+
+pub fn initialize() -> io::Result<Files> {
     let mut files = Files {
         config_root: "./config/".into(),
         config_files: Vec::new(),
         data_root: "./data/".into(),
-        data_files: Vec::new(),
+        _data_files: Vec::new(),
     };
     for directory in
     [files.config_root.clone(), files.data_root.clone()] {
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use player_io::{Command, Nav};
 
 fn main() {
     let position = navigation::Position::new(0, 0, false);
-    let _files = file_io::initialize();
-    let cmd_ifc = player_io::initialize_commands();
+    let files = file_io::initialize().unwrap();
+    let commands = player_io::initialize_commands();
+    for line in files.read_lines_of("commands").unwrap() {
+        println!("{}", line);
+    };
     
     loop {
-        let (command, _args) = cmd_ifc.prompt(
+        let (command, _args) = commands.prompt(
             &format!("you are at {}", position)
         ).unwrap();
         match command {
